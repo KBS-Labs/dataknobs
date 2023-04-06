a99d2ae6ba1bec9883a75491a542e5f4aa95ed96
Spencer Koehler <spencer.koehler@rivethealth.com>
2023-04-05 18:46:23 -0600

    updates

--- dataknobs/utils/file_utils.py
+++ dataknobs/utils/file_utils.py
@@ -2,33 +2,6 @@ import os
 from typing import Set
 
 
-def split_wget_fpath(fpath: str):
-    '''
-    Split the filepath into parts in cases where files may be named from e.g.,
-    wget and may have query strings in the name.
-
-    :param fpath: The file path to split
-    :return: (dirpath, basename, ext, query)
-    '''
-    query = ''
-    dirpath, fname = os.path.split(fpath)
-    basename, ext = os.path.splitext(fname)
-    if ext.startswith('.gz?'):
-        query = '.' + ext[3:]
-        ext = '.gz'
-    if ext == '.gz':
-        basename, ext = os.path.splitext(basename)
-    return (dirpath, basename, ext, query)
-
-
-def get_norm_ext(fpath: str):
-    '''
-    Get the normalized (lowercase) extension of the file, accounting for files
-    named from e.g., wget having query strings in the name.
-    '''
-    return split_wget_fpath(fpath)[2].lower()
-
-
 def filepath_generator(
         rootpath: str,
         descend: bool = True,
@@ -40,6 +13,7 @@ def filepath_generator(
     :param rootpath: The root path under which to find files.
     :param descend: True to descend into subdirectories
     :param seen: Set of filepaths and/or directories to ignore
+    :yield: Each file path
     '''
     if seen is None:
         seen = set()
@@ -56,3 +30,36 @@ def filepath_generator(
                 if next_root not in seen:
                     seen.add(next_root)
                     filepath_generator(next_root, descend=True, seen=seen)
+
+
+def fileline_generator(filename: str, rootdir: str = None):
+    '''
+    Generate lines from the file.
+    :param filename: The name of the file.
+    :param rootdir: (optional) The directory of the file.
+    :yield: Each stripped file line
+    '''
+    if filename.endswith('.gz'):
+        open_fn = gzip.open
+        mode = 'rt'
+    else:
+        open_fn = open
+        mode = 'r'
+    if rootdir is not None:
+        filename = os.path.join(rootdir, filename)
+    with open_fn(filename, mode=mode, encoding='utf-8') as f:
+        for line in f:
+            yield line.strip()
+
+
+def write_lines(outfile, lines, rootdir=None):
+    '''
+    Write the lines to the file.
+    :param outfile: The name of the file.
+    :param rootdir: (optional) The directory of the file.
+    '''
+    if rootdir is not None:
+        outfile = os.path.join(rootdir, outfile)
+    with open(outfile, 'w', encoding='utf-8') as f:
+        for line in sorted(lines):
+            print(line, file=f)
