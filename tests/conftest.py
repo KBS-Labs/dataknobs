6300375dcdf6564814701bb1885fe5b436ba5f75
Spencer B. Koehler <KoehlerSB747@gmail.com>
2023-08-03 17:14:27 -0600

    removed extraneous fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -87,25 +87,10 @@ def test_json_001() -> Dict:
 
 
 @pytest.fixture
-def test_json_001gz() -> Dict:
-    return resource_as_text('utils', TEST_JSON_001GZ)
-
-
-@pytest.fixture
 def test_json_002() -> Dict:
     return resource_as_text('utils', TEST_JSON_002)
 
 
 @pytest.fixture
-def test_json_002gz() -> Dict:
-    return resource_as_text('utils', TEST_JSON_002GZ)
-
-
-@pytest.fixture
 def test_json_003() -> Dict:
     return resource_as_text('utils', TEST_JSON_003)
-
-
-@pytest.fixture
-def test_json_003gz() -> Dict:
-    return resource_as_text('utils', TEST_JSON_003GZ)
